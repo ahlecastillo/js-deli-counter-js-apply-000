28c5d4ea6f1006aa8e2bcaffabf57a55336f6fab
Alejandra Castillo <ahle.castillo@gmail.com>
2019-01-09 21:15:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ return "Welcome, " + name + ". You are number " + line.length + " in line.";
 function nowServing (line) {
 
   if (line.length === 0) {
-    return "There is nobody waiting to be served!"; 
+    return "There is nobody waiting to be served!";
+    
   } else {
     return "Currently serving " + line.shift() + "."; 
   }
